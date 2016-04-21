c2bf9b6e0486f346d576f738e2ab824d98016e31
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-04-21 22:01:48 +0700

    tambah model kriteria

---
+++ application/models/Kriteria.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ *
+ * Author Rizki Mufrizal <mufrizalrizki@gmail.com>
+ * Since Apr 21, 2016
+ * Time 9:46:01 PM
+ * Encoding UTF-8
+ * Project Metode-SAW
+ * Package Expression package is undefined on line 14, column 14 in Templates/Scripting/PHPClass.php.
+ * 
+ */
+class Kriteria extends CI_Model {
+
+    public function tambahKriteria($kriteria) {
+        $this->db->insert('tb_kriteria', $kriteria);
+    }
+
+    public function ambilKriteria() {
+        return $this->db->get('tb_kriteria')->result();
+    }
+
+}
