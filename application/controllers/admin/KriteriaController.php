3619c417d2067ea57f324c6fe6bb6e4bb275a81a
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-04-21 22:02:02 +0700

    tambah controller kriteria

---
+++ application/controllers/admin/KriteriaController.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ *
+ * Author Rizki Mufrizal <mufrizalrizki@gmail.com>
+ * Since Apr 21, 2016
+ * Time 9:47:30 PM
+ * Encoding UTF-8
+ * Project Metode-SAW
+ * Package Expression package is undefined on line 14, column 14 in Templates/Scripting/PHPClass.php.
+ * 
+ */
+class KriteriaController extends CI_Controller {
+
+    public function __construct() {
+        parent::__construct();
+        $this->load->model('Kriteria');
+    }
+
+    public function index() {
+        $data['kriteria'] = $this->Kriteria->ambilKriteria();
+        $this->load->view('admin/KriteriaView', $data);
+    }
+
+    public function tambahKriteria() {
+        $val = array(
+            'id_kriteria' => $this->uuid->v4(),
+            'kriteria' => $this->input->post('kriteria'),
+            'bobot' => $this->input->post('bobot')
+        );
+        $this->Kriteria->tambahKriteria($val);
+        redirect('admin/KriteriaController');
+    }
+
+}
