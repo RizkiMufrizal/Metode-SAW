f44b36a69a922766aacde7ae515adc09f9040635
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-04-23 00:23:38 +0700

    update model nilai calon siswa

--- application/models/NilaiCalonSiswa.php
+++ application/models/NilaiCalonSiswa.php
@@ -16,8 +16,26 @@ class NilaiCalonSiswa extends CI_Model {
         return $this->db->get('tb_calon_siswa_nilai')->result();
     }
 
+    public function ambilNilaiCalonSiswaArray() {
+        return $this->db->get('tb_calon_siswa_nilai')->result_array();
+    }
+
     public function tambahNilaiCalonSiswa($nilaiCalonSiswa) {
         $this->db->insert('tb_nilai_calon_siswa', $nilaiCalonSiswa);
+    }
+
+    public function ambilNilaiCalonSiswaBerdasakanNim($nim) {
+        $this->db->where('nim', $nim);
+        return $this->db->count_all_results('tb_calon_siswa_nilai');
+    }
+
+    public function ambilJumlahNilaiCalonSiswa() {
+        return $this->db->count_all_results('tb_nilai_calon_siswa');
+    }
+
+    public function ambilNilaiMaxBerdasarkanKriteria($kriteria) {
+        $this->db->select_max($kriteria);
+        return $this->db->get('tb_nilai_calon_siswa')->result_array();
     }
 
 }
