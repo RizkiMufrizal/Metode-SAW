2780010a228ac6ce19f02a8dc0ff73411d45894d
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-04-24 18:47:04 +0700

    update saw.sql

--- saw.sql
+++ saw.sql
@@ -27,6 +27,32 @@ CREATE TABLE tb_calon_siswa(
     status BOOLEAN DEFAULT FALSE
 )ENGINE=INNODB;
 
+CREATE TABLE tb_himpunan(
+    id_himpunan VARCHAR(150) NOT NULL PRIMARY KEY,
+    batas_atas INTEGER NOT NULL,
+    batas_bawah INTEGER NOT NULL,
+    nilai FLOAT NOT NULL
+)ENGINE=INNODB;
+
+insert into tb_himpunan(id_himpunan, batas_atas, batas_bawah, nilai)
+    values('1', 0, 20, 0.2);
+
+
+insert into tb_himpunan(id_himpunan, batas_atas, batas_bawah, nilai)
+    values('2', 21, 40, 0.4);
+
+
+insert into tb_himpunan(id_himpunan, batas_atas, batas_bawah, nilai)
+    values('3', 41, 60, 0.6);
+
+
+insert into tb_himpunan(id_himpunan, batas_atas, batas_bawah, nilai)
+    values('4', 61, 80, 0.8);
+
+
+insert into tb_himpunan(id_himpunan, batas_atas, batas_bawah, nilai)
+    values('5', 81, 100, 1);
+
 CREATE TABLE tb_nilai_calon_siswa(
     id_nilai VARCHAR(150) NOT NULL PRIMARY KEY,
     c1 FLOAT NOT NULL,
@@ -34,6 +60,11 @@ CREATE TABLE tb_nilai_calon_siswa(
     c3 FLOAT NOT NULL,
     c4 FLOAT NOT NULL,
     c5 FLOAT NOT NULL,
+    nilai_asli_c1 FLOAT NOT NULL,
+    nilai_asli_c2 FLOAT NOT NULL,
+    nilai_asli_c3 FLOAT NOT NULL,
+    nilai_asli_c4 FLOAT NOT NULL,
+    nilai_asli_c5 FLOAT NOT NULL,
     nim VARCHAR(50) NOT NULL,
     FOREIGN KEY(nim) REFERENCES tb_calon_siswa(nim)
 )ENGINE=INNODB;
