21ed81d5f2fa48daa2f703f024c4df70f972245c
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-04-24 21:55:38 +0700

    update model himpunan

--- application/models/Himpunan.php
+++ application/models/Himpunan.php
@@ -1,15 +1,14 @@
 <?php
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
- * Description of Himpunan1
  *
- * @author rizki
+ * Author Rizki Mufrizal <mufrizalrizki@gmail.com>
+ * Since Apr 24, 2016
+ * Time 8:31:32 PM
+ * Encoding UTF-8
+ * Project Metode-SAW
+ * Package Expression package is undefined on line 13, column 14 in Templates/Scripting/PHPClass.php.
+ * 
  */
 class Himpunan extends CI_Model {
 
