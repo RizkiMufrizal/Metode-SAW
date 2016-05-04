fc2e5ba9278781e1ed5e18b9cd6451e5779b2ae8
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-05-04 21:34:08 +0700

    tambah upload untuk calon dan nilai siswa

--- application/controllers/admin/CalonSiswaController.php
+++ application/controllers/admin/CalonSiswaController.php
@@ -14,8 +14,10 @@ class CalonSiswaController extends CI_Controller {
 
     public function __construct() {
         parent::__construct();
+        $this->load->library('CSVReader');
         $this->load->model('CalonSiswa');
         $this->load->model('NilaiCalonSiswa');
+        $this->load->model('Himpunan');
     }
 
     public function index() {
@@ -24,6 +26,7 @@ class CalonSiswaController extends CI_Controller {
         if ($session == FALSE) {
             redirect('admin/login');
         } else {
+            //$data['error'] = array('error' => '');
             $data['calon_siswa'] = $this->CalonSiswa->ambilCalonSiswa();
             $this->load->view('admin/CalonSiswaView', $data);
         }
@@ -49,6 +52,92 @@ class CalonSiswaController extends CI_Controller {
     public function hapusCalonSiswa() {
         $this->CalonSiswa->hapusCalonSiswa();
         redirect('admin/CalonSiswaController');
+    }
+
+    public function uploadCsvCalonSiswa() {
+        $namaFile = $this->uuid->v4();
+        $config['upload_path'] = './uploads/';
+        $config['allowed_types'] = 'csv';
+        $config['max_size'] = 2000;
+        $config['file_name'] = $namaFile;
+        $this->load->library('upload', $config);
+
+        if (!$this->upload->do_upload('csv')) {
+            echo $this->upload->display_errors();
+        } else {
+            $file_data = $this->upload->data();
+            $file_path = './uploads/' . $file_data['file_name'];
+
+            $result = $this->csvreader->parse_file($file_path);
+
+            //simpan calon siswa
+            foreach ($result as $row) {
+                $val = array(
+                    'nim' => $row['nim'],
+                    'nama' => $row['nama'],
+                    'jenis_kelamin' => $row['jenis_kelamin'],
+                    'tanggal_lahir' => $row['tanggal_lahir'],
+                    'alamat' => $row['alamat']
+                );
+                $this->CalonSiswa->tambahCalonSiswa($val);
+            }
+
+            //simpan calon siswa
+            foreach ($result as $row) {
+                $nim = $row['nim'];
+                $c1 = $row['nilai_psikotes'];
+                $c2 = $row['nilai_psm_test'];
+                $c3 = $row['nilai_angket_siswa'];
+                $c4 = $row['nilai_un'];
+                $c5 = $row['nilai_raport'];
+
+                foreach ($this->Himpunan->ambilHimpunan() as $h) {
+                    if ($c1 >= $h->batas_atas and $c1 <= $h->batas_bawah) {
+                        $c1 = $h->nilai;
+                    }
+
+                    if ($c2 >= $h->batas_atas and $c2 <= $h->batas_bawah) {
+                        $c2 = $h->nilai;
+                    }
+
+                    if ($c3 >= $h->batas_atas and $c3 <= $h->batas_bawah) {
+                        $c3 = $h->nilai;
+                    }
+
+                    if ($c4 >= $h->batas_atas and $c4 <= $h->batas_bawah) {
+                        $c4 = $h->nilai;
+                    }
+
+                    if ($c5 >= $h->batas_atas and $c5 <= $h->batas_bawah) {
+                        $c5 = $h->nilai;
+                    }
+                }
+
+                $val = array(
+                    'id_nilai' => $this->uuid->v4(),
+                    'nilai_asli_c1' => $row['nilai_psikotes'],
+                    'nilai_asli_c2' => $row['nilai_psm_test'],
+                    'nilai_asli_c3' => $row['nilai_angket_siswa'],
+                    'nilai_asli_c4' => $row['nilai_un'],
+                    'nilai_asli_c5' => $row['nilai_raport'],
+                    'c1' => $c1,
+                    'c2' => $c2,
+                    'c3' => $c3,
+                    'c4' => $c4,
+                    'c5' => $c5,
+                    'nim' => $nim
+                );
+
+                $this->NilaiCalonSiswa->tambahNilaiCalonSiswa($val);
+
+                $valCalonSiswa = array(
+                    'status' => TRUE
+                );
+                $this->CalonSiswa->ubahCalonSiswa($valCalonSiswa, $nim);
+            }
+
+            redirect('admin/CalonSiswaController');
+        }
     }
 
 }
