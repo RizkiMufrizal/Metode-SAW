5cd93e57db10b1ae563a92433ec37f6fd18077b7
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-05-28 19:13:06 +0700

    hapus himpunan

--- application/controllers/admin/NilaiCalonSiswaController.php
+++ application/controllers/admin/NilaiCalonSiswaController.php
@@ -15,7 +15,6 @@ class NilaiCalonSiswaController extends CI_Controller
         parent::__construct();
         $this->load->model('NilaiCalonSiswa');
         $this->load->model('CalonSiswa');
-        $this->load->model('Himpunan');
     }
 
     public function index()
@@ -39,35 +38,8 @@ class NilaiCalonSiswaController extends CI_Controller
         $c4 = $this->input->post('c4');
         $c5 = $this->input->post('c5');
 
-        foreach ($this->Himpunan->ambilHimpunan() as $h) {
-            if ($c1 >= $h->batas_atas and $c1 <= $h->batas_bawah) {
-                $c1 = $h->nilai;
-            }
-
-            if ($c2 >= $h->batas_atas and $c2 <= $h->batas_bawah) {
-                $c2 = $h->nilai;
-            }
-
-            if ($c3 >= $h->batas_atas and $c3 <= $h->batas_bawah) {
-                $c3 = $h->nilai;
-            }
-
-            if ($c4 >= $h->batas_atas and $c4 <= $h->batas_bawah) {
-                $c4 = $h->nilai;
-            }
-
-            if ($c5 >= $h->batas_atas and $c5 <= $h->batas_bawah) {
-                $c5 = $h->nilai;
-            }
-        }
-
         $val = array(
             'id_nilai' => $this->uuid->v4(),
-            'nilai_asli_c1' => $this->input->post('c1'),
-            'nilai_asli_c2' => $this->input->post('c2'),
-            'nilai_asli_c3' => $this->input->post('c3'),
-            'nilai_asli_c4' => $this->input->post('c4'),
-            'nilai_asli_c5' => $this->input->post('c5'),
             'c1' => $c1,
             'c2' => $c2,
             'c3' => $c3,
