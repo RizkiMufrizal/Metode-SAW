cc33a3cd13ffa9b6aea76da627f4ee282bf525b6
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-05-28 19:12:30 +0700

    format model kriteria

--- application/models/Kriteria.php
+++ application/models/Kriteria.php
@@ -8,33 +8,38 @@
  * Project Metode-SAW
  * Package Expression package is undefined on line 14, column 14 in Templates/Scripting/PHPClass.php.
  */
-class Kriteria extends CI_Model {
-
-    public function tambahKriteria($kriteria) {
+class Kriteria extends CI_Model
+{
+    public function tambahKriteria($kriteria)
+    {
         $this->db->insert('tb_kriteria', $kriteria);
     }
 
-    public function ambilKriteria() {
+    public function ambilKriteria()
+    {
         $this->db->order_by('kriteria', 'asc');
 
         return $this->db->get('tb_kriteria')->result();
     }
 
-    public function ambilKriteriaBerdasarkanId($idKriteria) {
+    public function ambilKriteriaBerdasarkanId($idKriteria)
+    {
         $this->db->where('id_kriteria', $idKriteria);
+
         return $this->db->get('tb_kriteria')->result();
     }
 
-    public function ubahKriteria($kriteria, $idKriteria) {
+    public function ubahKriteria($kriteria, $idKriteria)
+    {
         $this->db->where('id_kriteria', $idKriteria);
         $this->db->update('tb_kriteria', $kriteria);
     }
 
-    public function hapusKriteria($idKriteria) {
+    public function hapusKriteria($idKriteria)
+    {
         $kriteria = array(
-            'id_kriteria' => $idKriteria
+            'id_kriteria' => $idKriteria,
         );
         $this->db->delete('tb_kriteria', $kriteria);
     }
-
 }
